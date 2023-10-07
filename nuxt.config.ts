2fae2542a1167d84c602d9804c02134824b2e4cb
hds <hedongshu@foxmail.com>
2023-10-07 17:34:44 +0800

    feat: add app and style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,65 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "width=device-width,initial-scale=1",
+      title: "Hotuns's Blog",
+      titleTemplate: "%s - Hotuns's Blog",
+      meta: [{ name: "description", content: "Hotuns's awesome blog" }],
+    },
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "layout", mode: "out-in" },
+  },
+
+  sitemap: {
+    strictNuxtContentPaths: true,
+  },
+  site: {
+    url: "https://hotuns.me",
+    identity: {
+      type: "Person",
+    },
+    twitter: "@qdnvubp",
+  },
+
+  typescript: {
+    strict: true,
+    shim: false,
+  },
+
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/"],
+    },
+  },
+
+  colorMode: {
+    classSuffix: "",
+    preference: "dark",
+    fallback: "light",
+  },
+
+  modules: [
+    "@nuxt/image",
+    "nuxt-icon",
+    "@vueuse/nuxt",
+    "nuxt-og-image",
+    "@nuxt/content",
+    "@nuxtjs/robots",
+    "@nuxtseo/module",
+    "@nuxtjs/fontaine",
+    "@nuxtjs/color-mode",
+    "nuxt-simple-sitemap",
+    "@nuxtjs/tailwindcss",
+  ],
+
+  content: {
+    highlight: {
+      theme: "dracula",
+    },
+  },
+});
