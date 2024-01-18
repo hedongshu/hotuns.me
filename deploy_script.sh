a91942f4276535e74dced4590bebeff50d82ce45
hotuns <hedongshu@foxmail.com>
2024-01-18 14:53:31 +0800

    update ci

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+ARTIFACT_URL=$1  # Artifact URL 作为参数传递
+
 cd /home/ubuntu/hotuns.me
 # 下载构建文件
-wget https://github.com/hedongshu/hotuns.me/actions/artifacts/build.tar.gz
+wget -O build.tar.gz "$ARTIFACT_URL" --header='Authorization: token ${{ secrets.GITHUB_TOKEN }}'
 # 解压构建文件
 tar -xzf build.tar.gz
 # 重启您的服务，例如使用 pm2
