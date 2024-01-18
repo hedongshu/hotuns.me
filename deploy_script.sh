dacf09f1e5c2269d533e6ab276ad383ad3bb7543
hotuns <hedongshu@foxmail.com>
2024-01-18 16:02:53 +0800

    use zip

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,8 +3,8 @@ ARTIFACT_URL=$1  # Artifact URL 作为参数传递
 
 cd /home/ubuntu/hotuns.me
 # 下载构建文件
-wget -O build.tar.gz "$ARTIFACT_URL" --header='Authorization: token ${{ secrets.GITHUB_TOKEN }}'
+wget -O build.zip "$ARTIFACT_URL" --header='Authorization: token ${{ secrets.GITHUB_TOKEN }}'
 # 解压构建文件
-tar -xzf build.tar.gz
+unzip build.zip
 # 重启您的服务，例如使用 pm2
 pm2 restart all
