0392483d6131f50c58891bf583644a4f6e403141
hotuns <hedongshu@foxmail.com>
2024-01-18 14:17:19 +0800

    update ci

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /path/to/your/project
-git pull origin main
-npm install
-npm run build
+cd /home/ubuntu/hotuns.me
+# 下载构建文件
+wget https://github.com/hedongshu/hotuns.me/actions/artifacts/build.tar.gz
+# 解压构建文件
+tar -xzf build.tar.gz
 # 重启您的服务，例如使用 pm2
 pm2 restart all
