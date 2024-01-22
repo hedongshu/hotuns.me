51730f968927580983377f8cf38ab2f3d0186b84
hotuns <hedongshu@foxmail.com>
2024-01-22 09:39:11 +0800

    use sh -o

--- deploy_script.sh
+++ deploy_script.sh
@@ -10,11 +10,12 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
+  -o artifact.zip \
   https://api.github.com/repos/hedongshu/hotuns.me/actions/artifacts/"$ARTIFACT_ID"/zip
 
 
 # 解压构建文件
-unzip build.zip || { echo "解压失败"; exit 1; }
+unzip artifact.zip || { echo "解压失败"; exit 1; }
 
 # 重启您的服务，例如使用 pm2
 /home/ubuntu/.nvm/versions/node/v17.5.0/bin/pm2 restart all || { echo "PM2 重启失败"; exit 1; }
