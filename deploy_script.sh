a90e0f293cd2319fcbe0d9d4e366095cea2adc03
hotuns <hedongshu@foxmail.com>
2024-01-22 09:20:37 +0800

    use github api

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-ARTIFACT_URL=$1  # Artifact URL 作为参数传递
+ARTIFACT_ID=$1  # ARTIFACT_ID 作为参数传递
 
 cd /home/ubuntu/hotuns.me
 # 下载构建文件
-wget -O build.zip "$ARTIFACT_URL"  --header='Authorization: token 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' || { echo "下载失败"; exit 1; }
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/hedongshu/hotuns.me/actions/artifacts/"$ARTIFACT_ID"/zip
+
 
 # 解压构建文件
 unzip build.zip || { echo "解压失败"; exit 1; }
