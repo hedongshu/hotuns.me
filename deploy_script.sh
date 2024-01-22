ebd11750077693211e95ac0d4e4d21d458725d5c
hotuns <hedongshu@foxmail.com>
2024-01-22 09:29:12 +0800

    use env

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,10 +2,13 @@
 ARTIFACT_ID=$1  # ARTIFACT_ID 作为参数传递
 
 cd /home/ubuntu/hotuns.me
+# 加载 .env 文件
+source .env
+
 # 下载构建文件
 curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/hedongshu/hotuns.me/actions/artifacts/"$ARTIFACT_ID"/zip
 
